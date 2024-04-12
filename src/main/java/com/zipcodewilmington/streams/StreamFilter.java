0c58488f58abb6b0157c09e0e3a17d93b8d1f09c
Danny Cao <dannycao1997@yahoo.com>
2024-04-11 20:27:22 -0400

    Having issues with StreamFilers section, cannot pass tests. Reminder to come back and redo

--- src/main/java/com/zipcodewilmington/streams/StreamFilter.java
+++ src/main/java/com/zipcodewilmington/streams/StreamFilter.java
@@ -5,6 +5,7 @@ import com.zipcodewilmington.streams.anthropoid.PersonFactory;
 import com.zipcodewilmington.streams.tools.RandomUtils;
 import com.zipcodewilmington.streams.tools.StringUtils;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -20,15 +21,16 @@ public class StreamFilter {
      * No arg constructor
      */ //TODO - construct person stream of 100 person objects; startingCharacter is a random capital letter
     public StreamFilter() {
-        this(Stream.empty(), null);
-    }
+        this(new PersonFactory().createPersonStream(100), RandomUtils.createCharacter('A','Z'));
+    } // cannot get constructor test to pass, need to do more research
+
 
     /**
      * @param people - Array of person objects
      * @param startingCharacter - character to filter by
      */ //TODO
     public StreamFilter(Person[] people, Character startingCharacter) {
-        this(Stream.empty(), null);
+        this(Arrays.stream(people), startingCharacter);
     }
 
     /**
@@ -36,7 +38,7 @@ public class StreamFilter {
      * @param startingCharacter - character to filter by
      */ //TODO
     public StreamFilter(List<Person> people, Character startingCharacter) {
-        this(Stream.empty(), null);
+        this(people.stream(), startingCharacter);
     }
 
 
